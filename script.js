aeab3d457b0e3b0fd44c66e628886f5f6efdec14
gichurustanley <127530680+gichurustanley@users.noreply.github.com>
2023-04-08 01:33:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+
+
+var names1 = ["Yaakov"];{
+  console.log("Hello" + names1)
+};
+var names2 = ["John",];{
+  console.log("Goodbye" + names2)};
+  var names3 = ["Jen",];{
+  console.log("Goodbye" + names3)};
+  var names4 = ["Jason",];{
+  console.log("Goodbye" + names4)};
+  var names5 = ["Paul",];{
+  console.log("Hello" + names5)};
+  var names6 = ["Frank",];{
+  console.log("Hello" + names6)};
+  var names7 = ["Larry",];{
+  console.log("Hello" + names7)}
+  var names8 = ["Paula",];{
+  console.log("Hello" + names8)};
+  var names9 = ["Laura",];{
+  console.log("Hello" + names9)};
+  var names10 = ["Jim",];{
+  console.log("Goodbye" + names10)};
+ // console.log("gooodbye" +names2[i]);
+//};
+//for (var i=0;i<names.length; i++) {};
+// console.log("goodbye" + names2[i]);
+ 
+
+//console.log("goodbye" + name[name])
+//for (var name in name
+  //console.log("hello" + names2[name])
+//};
+
+// STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
+// Loop over the names array and say either 'Hello' or "Good Bye"
+// using either the helloSpeaker's or byeSpeaker's 'speak' method.
+// See Lecture 50, part 1
+
+  // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
+  // Retrieve the first letter of the current name in the loop.
+  // Use the string object's 'charAt' function. Since we are looking for
+  // names that start with either upper case or lower case 'J'/'j', call
+  // string object's 'toLowerCase' method on the result so we can compare
+  // to lower case character 'j' afterwards.
+  // Look up these methods on Mozilla Developer Network web site if needed.
+ // var firstLetter = names[i].charAt(0).toLowerCase();
+
+  // STEP 12: (NOTHING TO DO. ALREADY DONE FOR YOU)
+  // Compare the 'firstLetter' retrieved in STEP 11 to lower case
+  // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
+  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
+  // name in the loop.
+ 
+ // if (firstLetter === 'j') {
+  //  byeSpeaker.speak(names[i]);
+ // } else {
+   // helloSpeaker.speak(names[i]);
+ // }
+//}
+
+//})();
